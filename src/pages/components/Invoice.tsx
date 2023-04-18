5f9bfa6b7a9ecd44cf2677cb04f94f7d3ef6b205
Lance <lance.jiexi@gmail.com>
2023-04-18 17:57:58 +0200

    added invoice component

---
+++ src/pages/components/Invoice.tsx
@@ -0,0 +1,123 @@
+import React from "react";
+
+type InvoiceCardProps = {
+  id: string;
+  paymentDue: string;
+  clientName: string;
+  status: string;
+  total: number;
+};
+
+type invoiceProps = {
+  invoice: InvoiceCardProps[];
+};
+type InvoiceCard = {
+  invoice: InvoiceCardProps;
+};
+
+function Invoice({ invoice }: invoiceProps) {
+  return (
+    <div className="mb-20 mt-8 flex h-full w-full flex-col md:mt-14">
+      {invoice.map((invoice) => (
+        <InvoiceCard key={invoice.id} invoice={invoice} />
+      ))}
+    </div>
+  );
+}
+
+export default Invoice;
+
+export const InvoiceCard = ({ invoice }: InvoiceCard) => {
+  return (
+    <>
+      <CardMobile invoice={invoice} />
+      <CardLarge invoice={invoice} />
+    </>
+  );
+};
+
+export const Status = ({ status }: { status: string }) => {
+  const statusColor =
+    status === "paid"
+      ? "#33D69F"
+      : status === "pending"
+      ? "#FF8F00"
+      : "#DFE3FA";
+
+  return (
+    <div
+      className={`${
+        status === "paid"
+          ? "bg-[#33d69f]/10"
+          : status === "pending"
+          ? "bg-[#FF8F00]/10"
+          : "bg-[#DFE3FA]/10"
+      } flex  w-full max-w-[100px] items-center justify-center rounded-lg px-7 py-3`}
+    >
+      <span
+        className={`${
+          status === "paid"
+            ? "text-[#33D69F]"
+            : status === "pending"
+            ? "text-[#FF8F00]"
+            : "text-lavender"
+        } text-xs font-medium capitalize tracking-[-0.25px]`}
+      >
+        {status}
+      </span>
+    </div>
+  );
+};
+
+export const CardMobile = ({ invoice }: InvoiceCard) => {
+  return (
+    <div className="mt-4 flex  flex-col rounded-lg bg-primaryDarkBlue p-6 shadow-md md:hidden">
+      <div className="flex items-center justify-between">
+        <div className="flex items-center">
+          <span className="text-xs font-bold text-[#7E88C3]">#</span>
+          <span className="text-xs font-bold text-white">{invoice.id}</span>
+        </div>
+        <div className="text-xs font-medium tracking-[-0.25px] text-white">
+          {invoice.clientName}
+        </div>
+      </div>
+      <div className="flex items-center justify-between pt-6">
+        <div className="flex flex-col space-y-2">
+          <span className="text-xs font-medium tracking-[-0.25px] text-lavender">
+            Due {invoice.paymentDue}
+          </span>
+
+          <span className="text-base font-bold tracking-[-0.25px] text-lavender">
+            €{invoice.total.toFixed(2)}
+          </span>
+        </div>
+        <Status status={invoice.status} />
+      </div>
+    </div>
+  );
+};
+
+export const CardLarge = ({ invoice }: InvoiceCard) => {
+  return (
+    <div className="mt-4 hidden w-full items-center justify-between rounded-lg bg-primaryDarkBlue px-6 py-7 md:flex">
+      <div className="flex items-center">
+        <span className="text-xs font-bold text-[#7E88C3]">#</span>
+        <span className="text-xs font-bold text-white">{invoice.id}</span>
+      </div>
+      <div>
+        <span className="text-xs font-medium tracking-[-0.25px] text-lavender">
+          Due {invoice.paymentDue}
+        </span>
+      </div>
+      <div className="text-xs font-medium tracking-[-0.25px] text-white">
+        {invoice.clientName}
+      </div>
+      <div>
+        <span className="text-base font-bold tracking-[-0.25px] text-lavender">
+          €{invoice.total.toFixed(2)}
+        </span>
+      </div>
+      <Status status={invoice.status} />
+    </div>
+  );
+};
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,8 +2,13 @@ import { type NextPage } from "next";
 import Head from "next/head";
 import Link from "next/link";
 
+import data from "~/data.json";
+
 import { api } from "~/utils/api";
 import Navbar from "./components/Navbar";
+
+import { PlusIcon } from "@heroicons/react/24/solid";
+import Invoice from "./components/Invoice";
 
 const Home: NextPage = () => {
   const hello = api.example.hello.useQuery({ text: "from tRPC" });
@@ -16,7 +21,30 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Navbar />
-      <main className="flex min-h-screen flex-col bg-secondaryBlack"></main>
+      <main className="flex min-h-screen flex-col bg-secondaryBlack px-6">
+        <div className="flex w-full items-center justify-between  pt-9">
+          <div className="flex flex-col">
+            <h1 className="text-xl font-bold text-white">Invoices</h1>
+            <div className="pt-1 text-xs text-lavender">
+              <span className="block md:hidden">7 invoices</span>
+              <div className="hidden md:block">
+                There are <span>7</span> total invoices
+              </div>
+            </div>
+          </div>
+          <button className="flex items-center space-x-2 rounded-3xl bg-primaryPurple p-[6px] px-3">
+            <div className="flex h-8 w-8 items-center justify-center rounded-full bg-white">
+              <PlusIcon className="h-5 w-5 text-primaryPurple" />
+            </div>
+            <div className="text-white md:space-x-2">
+              <span className="text-xs font-bold">New</span>
+              <span className="hidden md:inline">Invoice</span>
+            </div>
+          </button>
+        </div>
+
+        <Invoice invoice={data} />
+      </main>
     </>
   );
 };
